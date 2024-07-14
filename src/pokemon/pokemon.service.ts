686a6f31acfc517c01d1436731138dc16601be01
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-14 11:45:17 -0500

    update pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -52,8 +52,14 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(query: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne( query );
+    if ( !pokemon ) throw new NotFoundException( `pokemon whit query "${query}" not found` );
+    if ( updatePokemonDto.name ) {
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+    }
+    await pokemon.updateOne( updatePokemonDto, { new: true });
+    return { ...pokemon.toJSON(), ...updatePokemonDto };
   }
 
   remove(id: number) {

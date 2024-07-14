5e962c172bb4ba966f00d35b2ad258ebb03d4c3d
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-14 13:13:25 -0500

    create seed data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return `This action returns a #seed`;
+  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {
+    
+  }
+
+
+  async executeSeed() {
+    const { data } = await  this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    data.results.forEach( async ( { name, url } ) => {
+      const segments = url.split('/');
+      const no = +segments[ segments.length - 2 ];
+      await this.pokemonModel.create({ name: name, no});
+    }); 
+    return 'seed executed';
   }
 }
